c7a99842e6649aa40674fcebdc987ffbb3c48fb1
aleebrady <aleebrady@gmail.com>
2021-04-12 13:47:03 -0700

    added component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
@@ -7,6 +8,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, This is a new React App</h1>
         <p>This is working</p>
+        <Person />
       </div>
     );
     // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Does this work now?'))
