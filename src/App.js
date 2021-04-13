e0cb61bbc154ad1ae6aebd7246e919688d3ecc15
aleebrady <aleebrady@gmail.com>
2021-04-13 14:17:20 -0700

    setState for switchHandler function

--- src/App.js
+++ src/App.js
@@ -3,14 +3,36 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Aaron', age: 36 },
+      { name: 'Bob', age: 30 },
+      { name: 'Tom', age: 31 }
+    ],
+    otherState: 'Other value'
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked')
+    // this.state.persons[0].name = 'This is the first name' - Incorrect
+    this.setState({
+      persons: [
+        { name: 'Aaron Lee', age: 36 },
+        { name: 'Bob', age: 30 },
+        { name: 'Tom', age: 39 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, This is a new React App</h1>
         <p>This is working</p>
-        <Person name="Aaron" age="36" />
-        <Person name="Bob" age="30">My Hobbies are: Cards</Person>
-        <Person name="Tim" age="34" />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies are: Cards</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Does this work now?'))
